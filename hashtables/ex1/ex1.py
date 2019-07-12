3dd1e21e94208f4de692353f61dce12b39b4aaa6
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-07-12 12:49:43 -0700

    answered both exercise 1 and 2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -6,12 +6,40 @@ from hashtables import (HashTable,
                         hash_table_resize)
 
 
+# === ALWAYS RETURN LARGER INDEX FIRST TO PASS TESTS
+
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    if length < 2:
+        return None
+    elif length == 2:
+        if weights[1] + weights[0] == limit:
+            print("RETURNING: ", )
+            return [1, 0]
+        else:
+            return None
+    else:
+        # add all array items to the hash table
+        for i in range(len(weights)):
+            hash_table_insert(ht, weights[i], i)
+        
+        for item in range(0, limit+1):
+            max = limit - item
+
+            low_num = hash_table_retrieve(ht, item)
+            print("LOW NUM: ", low_num)
+            if low_num is not None:
+                hash_table_remove(ht, item)
+                high_num = hash_table_retrieve(ht, max)
+                print("HIGH NUM: ", high_num)
+                if high_num is not None:
+                    print("RETURNING", [high_num, low_num])
+                    return [high_num, low_num]
+        return None
+
+
+
 
     return None
 
